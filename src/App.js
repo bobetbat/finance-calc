97b40f17ac594e8c29ff46c207edd072800595c6
Batyr <bobetbat@Regiv-iMac.local>
2020-04-27 21:02:57 +0200

    main set up: webpack + babel/ mobx + legacy decorators/

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { inject } from 'mobx-react'
 
 import AccountList from './components/AccountList'
-import NavRoutes from './components/NavRoutes'
 import MainContainer from './components/MainContainer'
+
 @inject('Store')
 class App extends React.Component {
   render() {
